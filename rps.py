cfe4244dd9a1c04cd0e9b5c542731cf265f6c232
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-02-27 11:03:27 +1300

    add a while loop

--- rps.py
+++ rps.py
@@ -3,8 +3,9 @@ import random
 user_score = 0
 cpu_score = 0
 
-user_choice = input('Choose rock, paper or scissors: ').lower()
-cpu_choice = random.choice(["rock", "paper", "scissors"])
+while user_score <= 10 and cpu_score <= 10:
+    user_choice = input('Choose rock, paper or scissors: ')
+    cpu_choice = random.choice(["rock", "paper", "scissors"])
 
-if user_choice == "rock" and cpu_choice == "scissors":
-    user_score += 1
+    if user_choice == "rock" and cpu_choice == "scissors":
+        user_score += 1
