b8e783060e2e4b686168bbc0365ef833b12b3cb5
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-02-27 11:05:10 +1300

    add print

--- rps.py
+++ rps.py
@@ -8,4 +8,6 @@ while user_score <= 10 and cpu_score <= 10:
     cpu_choice = random.choice(["rock", "paper", "scissors"])
 
     if user_choice == "rock" and cpu_choice == "scissors":
+        print('You win this round!')
         user_score += 1
+        print(f'Score: You: {user_score}, CPU: {cpu_score}')
